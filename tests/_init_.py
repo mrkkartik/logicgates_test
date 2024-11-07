9a9c01de217e63b145b01ca6681ead445ff780be
mrkkartik <167387492+mrkkartik@users.noreply.github.com>
2024-11-06 21:43:57 -0500

    Create _init_.py

---
+++ tests/_init_.py
@@ -0,0 +1,14 @@
+import pytest
+
+from logic_gates.gates import (
+    LogicGate,
+    BinaryGate,
+    UnaryGate,
+    ANDGate,
+    ORGate,
+    NOTGate,
+    NANDGate,
+    NORGate,
+    XORGate,
+    Connector,
+)
